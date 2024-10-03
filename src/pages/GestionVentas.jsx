784d838b922bacebdf97f2e98952a69e8f94bfa9
Levi Cespedes <leje_04@hotmail.com>
2024-10-03 14:52:28 -0500

    ...

--- src/pages/GestionVentas.jsx
+++ src/pages/GestionVentas.jsx
@@ -9,17 +9,6 @@ function GestionVentas() {
     <div className="ventas-container">
       <h2>Gestión de Ventas</h2>
       <p>Este es el módulo donde puedes gestionar las ventas del restaurante.</p>
-
-      <div className="charts-container">
-        <div className="chart">
-          <h3>Ventas del Mes (Gráfico de Barras)</h3>
-          <BarChart />
-        </div>
-        <div className="chart">
-          <h3>Cantidad de Clientes (Gráfico Circular)</h3>
-          <PieChart />
-        </div>
-      </div>
     </div>
   );
 }
